d40b1a8f79135cba4a6cb895636c13eb06d01595
glebTTA <gleb@techtrainasia.com>
2024-12-20 13:56:10 +0700

    Update lesson_one_score.lua

--- lesson_one_score.lua
+++ lesson_one_score.lua
@@ -1,1 +1,15 @@
+-- This is a script for my game
 
+print("The Game is Starting...")
+
+-- Now, we will create a variable to keep track of our score
+
+_G.score = 0
+
+-- This code will help us add 1 point to our score each second
+
+while true do
+	wait(1) -- wait 1 sesond before adding a point
+	score = score + 1 -- add 1 point to our score
+	print(score)
+end
